eb044ffabac7c9f20f837f0519451293201e9bd4
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-03-17 17:42:05 -0300

    update

---
+++ java.js
@@ -0,0 +1,19 @@
+let ligar = document.getElementById("ligar")
+let desligar = document.getElementById("desligar")
+
+function ligar(){
+
+}
+
+function desligar(){
+    
+}
+
+ligar.addEventListener("click",()=>{
+
+})
+
+
+desligar.addEventListener("click",()=>{
+
+})
